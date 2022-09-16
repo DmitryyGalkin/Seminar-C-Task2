3f10920e0364e0a99c8e0f5afe01264b8c514b2a
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-16 15:06:24 +0300

    task2_max_min

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+string input1 = Console.ReadLine()!;
+int number1 = int.Parse(input1);
+
+Console.WriteLine("Введите второе число: ");
+string input2 = Console.ReadLine()!;
+int number2 = int.Parse(input2);
+
+if (number1>number2)
+{
+Console.Write("max = ");
+Console.Write(number1);
+Console.Write(", min = ");
+Console.WriteLine(number2);
+}
+else
+{
+Console.Write("max = ");
+Console.Write(number2);
+Console.Write(", min = ");
+Console.WriteLine(number1);
+}
